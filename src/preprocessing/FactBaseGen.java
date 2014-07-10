2b911928c1ee1b9ec800aac019a0c0052c720565
andycyrus <andylee@AndydeMacBook-Pro.local>
2014-07-10 17:47:35 +0800

    code modification

--- src/preprocessing/FactBaseGen.java
+++ src/preprocessing/FactBaseGen.java
@@ -1,5 +1,16 @@
 package preprocessing;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+
 public class FactBaseGen {
 
 }
